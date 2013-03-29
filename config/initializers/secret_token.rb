10d5142228328fb9b831cdb3b1a8342e9c6f1a32
Shuxin (Sushi) Zhan <shuxinzhan@gmail.com>
2013-03-29 19:09:09 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Musicator::Application.config.secret_token = '72cb89b4867dbf2b8fece130eea92dadd858fa8c8310599f76a78f9a15f531f87f1bbbb326c9e6e8097671d9f24993e04f59cbba375130bad690b9ea30054323'
