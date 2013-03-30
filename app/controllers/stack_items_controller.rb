245689484d44852bdb46e69bd00aaa720eb417ee
Heinrich Jiang <heinrich.jiang@gmail.com>
2013-03-30 12:02:52 -0400

    submissions view

--- app/controllers/stack_items_controller.rb
+++ app/controllers/stack_items_controller.rb
@@ -2,13 +2,14 @@ class StackItemsController < ActionController::Base
 	
 	def create
 		@sub = Submission.find(params[:stack_item][:sub_id].to_s)
-		@sub.stack_items.create(comment: params[:stack_item][:comment])
+		@sub.stack_items.create(comment: params[:stack_item][:comment],
+                            author_id: params[:stack_item][:author_id])
 		redirect_to @sub
 	end
 
 
 	def new
-		@sub = Submission.find(params[:submission_id])
+    @sub = Submission.find(params[:submission_id])
 		@comment = @sub.stack_items.build()
 	end
 end
--- app/models/stack_item.rb
+++ app/models/stack_item.rb
@@ -1,6 +1,8 @@
 class StackItem
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :comment, type: String
+  field :author_id, type: Moped::BSON::ObjectId
   embedded_in :submission
   embeds_many :comments
   embeds_many :highlights
