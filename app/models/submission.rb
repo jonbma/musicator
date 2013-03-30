797cd153c4a669b392049d8028aa84f1a8d8d074
Heinrich Jiang <heinrich.jiang@gmail.com>
2013-03-30 03:43:46 -0400

    added timestamps to submissions

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,8 +1,11 @@
 class Submission
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :author_id, type: Moped::BSON::ObjectId
   field :reviewer_id, type: Moped::BSON::ObjectId
   field :name, type: String
+  field :datetime, type: DateTime
   embeds_many :stack_items
-  embedded_in :user
+
+
 end
