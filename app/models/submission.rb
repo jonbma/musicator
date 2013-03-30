f9b0a7c713a1e37d5e5dc53829c5e3dc7e1d947f
Heinrich Jiang <heinrich.jiang@gmail.com>
2013-03-30 01:20:11 -0400

    dashboard

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,5 +1,5 @@
 class Submission
   include Mongoid::Document
-  field :author_id, type: Integer
-  field :receiver_id, type: Integer
+  field :author_id, type: Moped::BSON::ObjectId
+  field :receiver_id, type: Moped::BSON::ObjectId
 end
