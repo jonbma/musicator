1ca39fa98ea7ebc2cebb1a054019d8cdc5b083b1
Heinrich Jiang <heinrich.jiang@gmail.com>
2013-03-30 03:28:32 -0400

    dashboard table

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,7 +1,7 @@
 class Submission
   include Mongoid::Document
   field :author_id, type: Moped::BSON::ObjectId
-  field :receiver_id, type: Moped::BSON::ObjectId
+  field :reviewer_id, type: Moped::BSON::ObjectId
   field :name, type: String
   embeds_many :stack_items
   embedded_in :user
